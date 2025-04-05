9a1431a42d0d79da3b394fa5d46ffc986c882950
Llarence <1010kai.t1010@gmail.com>
2025-04-05 13:59:53 -0700

    add firebase

---
+++ src/lib/server/index.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAusGwxakO9YGF1s411bjrtoHQGOL1029w",
+    authDomain: "quizlet-royale.firebaseapp.com",
+    projectId: "quizlet-royale",
+    storageBucket: "quizlet-royale.firebasestorage.app",
+    messagingSenderId: "287942623688",
+    appId: "1:287942623688:web:ba3196ca83012eaece40fd",
+    measurementId: "G-7VWZJW56J5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
