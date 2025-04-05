9c4ab47cdb5c643f00d6b76fbf5fe463d51ec377
AidanSchmitigal <129339235+AidanSchmitigal@users.noreply.github.com>
2025-04-05 14:12:35 -0700

    Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
+
+<header class="flex items-center justify-between bg-neutral-100 p-4">
+	<h1>Flashcard Royale</h1>
+	<nav>
+		<ul class="flex gap-4">
+			<li>
+				<a href="/play" class="{page.url.pathname === '/play' ? 'underline' : ''} text-neutral-500">
+					Play
+				</a>
+			</li>
+			<li>
+				<a
+					href="/leaderboard"
+					class="{page.url.pathname === '/play' ? 'underline' : ''} text-neutral-500"
+				>
+					Leaderboard
+				</a>
+			</li>
+			<li>
+				<a
+					href="/profile/aidan"
+					class="{page.url.pathname === '/play' ? 'underline' : ''} text-neutral-500"
+				>
+					Profile
+				</a>
+			</li>
+		</ul>
+	</nav>
+</header>
 
 {@render children()}
