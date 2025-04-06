14ae529a60659dc1717ff73cc1ff6f121bdf945d
AidanSchmitigal <129339235+AidanSchmitigal@users.noreply.github.com>
2025-04-05 17:08:10 -0700

    right

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 
-	const { children } = $$props;
+	let { children } = $props();
 </script>
 
 <div class="app-container">
