54720d02d59a6e44beec4382d845dbbae4aac6cc
Llarence <1010kai.t1010@gmail.com>
2025-04-05 17:51:21 -0700

    add decks

--- src/lib/client/game/data.ts
+++ src/lib/client/game/data.ts
@@ -1,3 +1,27 @@
-async function createGame() {
-    
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import { db } from "../firebase";
+
+const games = collection(db, 'games');
+const decks = collection(db, 'decks')
+
+async function createDeck(deck: Deck) {
+    await setDoc(doc(decks, deck.id), deck)
+}
+
+async function getDeck(ownerId: string): Promise<Deck[] | null> {
+    try {
+        const docs = (await getDocs(query(decks, where(ownerId, 'in', 'ownerIds')))).docs
+        return docs.map((doc) => doc.data() as Deck)
+    } catch {
+        return null;
+    }
+}
+
+async function getGame(ownerId: string): Promise<Deck[] | null> {
+    try {
+        const docs = (await getDocs(query(decks, where(ownerId, 'in', 'ownerIds')))).docs
+        return docs.map((doc) => doc.data() as Deck)
+    } catch {
+        return null;
+    }
 }
--- src/lib/client/users.ts
+++ src/lib/client/users.ts
@@ -1,14 +1,11 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 import { db } from "./firebase";
 
 const users = collection(db, 'users');
 
 async function getName(id: string): Promise<string | null> {
     try {
-        const res = query(users, where('id', '==', id))
-
-        const doc = (await getDocs(res)).docs[0]
-        return doc.data['name']
+        return (await getDoc(doc(users, id))).data()!['name']
     } catch {
         return null;
     }
